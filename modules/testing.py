ae2280263a53200f737581bb61537a77fc8ba038
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-16 16:24:54 +0100

    Rewrite of testing module

--- modules/testing.py
+++ modules/testing.py
@@ -1,15 +1,37 @@
-import traceback
+import inspect
 import copy
+import sys
+import dyn
 
+
+def checkPythonVersion():
+    version = sys.version_info
+
+    if version.major < 3 or (version.major == 3 and version.minor < 5):
+        sys.exit("You need at least Python 3.5")
+
+checkPythonVersion()
+        
 class TestError(Exception):
     def __init__(self, message):
         super().__init__(message)
 
-class FailureError(TestError):
+class TestFailure(TestError):
     def __init__(self):
         super().__init__("Failing assertion")
 
+class InvalidTestCallError(TestError):
+    def __init__(self):
+        super().__init__("Calling test functions is disallowed")
+        
 class Score:
+    """
+    Represents a score.
+
+    Scores are not to be confused with fractions.
+    For example, a score of 5/10 is not the same as a score of 1/2.
+    Score addition also follows different rules.
+    """
     def __init__(self, value, maximum):
         assert 0 <= value, "Score value must be positive"
         assert value <= maximum, "Score value ({}) must not be greater than maximum ({})".format(value, maximum)
@@ -18,299 +40,296 @@ class Score:
         self.maximum = maximum
 
     def __add__(self, other):
+        """
+        Adds two scores together.
+
+        a/b + c/d = (a+c)/(b+d).
+        """
         return Score(self.value + other.value, self.maximum + other.maximum)
 
     def rescale(self, maximum):
+        """
+        Rescales the score to a given maximum.
+        """
         return Score(self.value / self.maximum * maximum, maximum)
 
     def __str__(self):
         return "{}/{}".format(self.value, self.maximum)
 
 
-class Test:
-    @classmethod
-    def factory(cls, *args, **kwargs):
-        return lambda parent: cls(parent, *args, *kwargs)
+class _Test:
+    """
+    Superclass for all test classes.
+    """
+    pass
 
-    def __init__(self, parent):
-        self.parent = parent
-
-    def context(self):
-        if self.parent is not None:
-            return self.parent.context()
-        else:
-            return []
+class _SingleChildTest(_Test):
+    def __init__(self):
+        super().__init__()
+        self._child = None
         
-    def __str__(self):
-        return "\n".join(self.context())
+    def addChild(self, child):
+        assert self._child == None, "Only one child allowed in {}, cannot add {}".format( type(self).__name__, type(child).__name__ )
+        self._child = child
 
-    
-class SingleChildTest(Test):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.child = None
+    def run(self):
+        return self._child.run()
+        
+
+class _RootTest(_SingleChildTest):
+    pass
+
+
+class _TestFunction(_Test):
+    def __init__(self, name, function):
+        self._name = name
+        self._function = function
+        self._environment = _testenv.copy()
+
+    def run(self):
+        global _testenv
+
+        if self._environment.skip:
+            self._environment.skippedTests.append(self._name)
+        else:
+            oldTestEnv = _testenv
+            try:
+                _testenv = self._environment
+                _testenv.push(testName = self._name)
+                self._function()
+                self._environment.passedTests.append(self._name)
+                return Score(1, 1)
+            except TestFailure:
+                self._environment.failedTests.append(self._name)
+                return Score(0, 1)
+            finally:
+                _testenv = oldTestEnv
+
+
+class _TestSuite(_Test):
+    def __init__(self):
+        super().__init__()
+        self._children = []
 
     def addChild(self, child):
-        assert self.child is None, "SingleChildTest received multiple children"
-        self.child = child
-        
-class TestSuite(Test):
-    """
-    Consists of subtests
-    """
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.children = []
-
-    def addChild(self, child):
-        self.children.append(child)
-
-class CumulativeTestSuite(TestSuite):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-    def weight(self):
-        return sum( [ child.weight() for child in self.children ] )
-        
-    def score(self):
-        assert len(self.children) > 0, "CumulativeTestSuite must have at least one child"
-
-        total = Score(0, 0)
-        
-        for child in self.children:
-            score = child.score()
-            total += score
-
-        return total.rescale(1)
-
-def cumulative():
-    return CumulativeTestSuite.factory()
+        self._children.append(child)
 
 
-class ConditionalTest(SingleChildTest):
-    """
-    Single child test is run only if condition is true.
-    """
-    def __init__(self, parent, condition):
-        super().__init__(parent)
-        self.condition = condition
-
-    def score(self):
-        if self.condition:
-            return self.child.score()
-        else:
-            return Score(0, self.child.weight())
-
-class FunctionTest(Test):
-    """
-    Calls the given function. If no exceptions are thrown,
-    returns score = 1, otherwise score = 0.
-    """
-    def __init__(self, parent, function):
-        super().__init__(parent)
-        self.function = function
-
-    def score(self):
-        try:
-            self.function(Ensure(self.parent))
-            return Score(1, 1)
-        except TestError:
-            return Score(0, 1)
-
-class ContextDecorator(SingleChildTest):
-    """
-    Adds a context message.
-    """
-    def __init__(self, parent, context):
-        super().__init__(parent)
-        self.__context = context
-
-    def score(self):
-        return self.child.score()
-
-    def context(self):
-        return self.parent.context() + [ self.__context ]
-
-
-def context(message, *args):
-    return ContextDecorator.factory(message.format(*args))
+class _CumulativeTestSuite(_TestSuite):
+    def run(self):
+        return sum( [ child.run() for child in self._children ], Score(0, 0) )
 
     
-class Scaler(SingleChildTest):
+class _Scaler(_SingleChildTest):
+    def __init__(self, maximum):
+        super().__init__()
+        self._maximum = maximum
+
+    def run(self):
+        return self._child.run().rescale(self._maximum)
+
+
+# Create root test
+_rootTest = _RootTest()
+
+# Create separate dynamic environment for tests
+_testenv = dyn.create()
+
+# Push a fresh frame with the root test in it
+# (necessary due to the root frame not being modifiable)
+_testenv.push(top=_rootTest, context=[], passedTests=[], failedTests=[], skippedTests=[], skip=False)
+
+
+def _dummy():
+    raise InvalidTestCallError()
+
+
+class _TestContextManager:
+    def __init__(self, *tests):
+        super().__init__()
+        self._tests = tests
+        
+    def __enter__(self):
+        for test in self._tests:
+            _testenv.top.addChild(test)
+            _testenv.push(top=test)
+
+    def __exit__(self, *args):
+        for test in self._tests:
+            _testenv.pop()
+
+
+def findFirst(xs, predicate):
+    for x in xs:
+        if predicate(x):
+            return x
+            
+def _firstExternalStackFrame():
     """
-    Scales the score
+    Returns the first external stack frame, i.e. the
+    first stack frame whose function does not reside
+    in this file.
     """
+    return findFirst(inspect.stack(), lambda frame: frame.filename != __file__)
+        
 
-    def __init__(self, parent, maximum):
-        super().__init__(parent)
-        self.__maximum = maximum
-
-    def score(self):
-        return self.child.score().rescale(self.__maximum)
-
-    def weight(self):
-        return self.__maximum
-
+cumulative = _TestContextManager(_CumulativeTestSuite())
 
 def scale(maximum):
-    return Scaler.factory(maximum)
+    return _TestContextManager(_Scaler(maximum))
+
+def testedModule(module = None):
+    """
+    If an argument is given, sets the dynamic variable testedModule
+    to the given argument. If no argument is given, returns the
+    current value of testedModule.
+    """
+    if module:
+        return _testenv.let(testedModule = module)
+    else:
+        return _testenv.testedModule
+
+def referenceModule(module = None):
+    """
+    If an argument is given, sets the dynamic variable referenceModule
+    to the given argument. If no argument is given, returns the
+    current value of referenceModule.
+    """
+    if module:
+        return _testenv.let(referenceModule = module)
+    else:
+        return _testenv.referenceModule
     
+def testedFunctionName(functionName = None):
+    """
+    If an argument is given, sets the dynamic variables testedFunctionName
+    to the given argument. It also sets testedFunction and referenceFunction by looking
+    it up in the current testedModule and referenceModule, respectively.
+    If no referenceModule is set, referenceFunction remains unmodified.
 
-# class ReferenceImplementation(Test):
-#     def __init__(self, parent):
-#         super().__init__(parent)
-
-#     def score(self):
-
-
-
-# class ReferenceImplementation:
-#     def __init__(self, context, reference, tested):
-#         self.context = context
-#         self.reference = reference
-#         self.tested = tested
+    If no argument is given, returns the
+    current value of testedFunctionName.
+    """
+    if functionName:
+        bindings = { "testedFunctionName": functionName }
         
-#     def addCase(self, *args, **kwargs):
-#         @block
-#         def test():
-#             testArgs = copy.deepcopy(args)
-#             testKwargs = copy.deepcopy(kwargs)
-#             refArgs = copy.deepcopy(args)
-#             refKwargs = copy.deepcopy(kwargs)
-            
-#             expected = self.reference(*refArgs, **refKwargs)
-#             actual = self.tested(*testArgs, **testKwargs)
-
-#             self.context.assertEquals(expected, actual)
-
-#         test()
+        assert _testenv.testedModule, "No testedModule set!"
+        bindings["testedFunction"] = getattr(_testenv.testedModule, functionName)
         
-#     def __enter__(self):
-#         return self
-        
-#     def __exit__(self, type, value, traceback):
-#         pass
+        # Only set referenceFunction if referenceModule is set
+        if _testenv.isBound("referenceModule"):
+            bindings["referenceFunction"] = getattr(_testenv.referenceModule, functionName)
+
+        return _testenv.let(**bindings)
+    else:
+        return _testenv.testedFunctionName
+
+def testedFunction():
+    return _testenv.testedFunction
+
+def referenceFunction():
+    return _testenv.referenceFunction
+
+def context(message, *args, **kwargs):
+    return _testenv.let(context = _testenv.context + [ message.format(*args, **kwargs) ])
+
+def test(name, *args, **kwargs):
+    """
+    Decorator that turns the function into a test
+    and adds it to the top test's children.
+    """
+    def wrapper(function):
+        # Add test function as child to top test
+        _testenv.top.addChild(_TestFunction(name.format(*args, **kwargs), function))
+        return _dummy
+
+    return wrapper
 
 
-class TestBuilder:
-    def __init__(self, module, root = Scaler(None, 20)):
-        self.module = module
-        self.__contextStack = [root]
+def fail():
+    errorStackFrame = _firstExternalStackFrame()
 
-    def __current(self):
-        return self.__contextStack[-1]
-
-    def __push(self, context):
-        self.__contextStack.append(context)
-
-    def __pop(self):
-        self.__contextStack.pop()
-
-    def child(self, function):
-        self.__current().addChild(function)
-        return function
-
-    def __helper(self, factory):
-        return TestBuilder.ContextStackHelper(self.__contextStack, factory)
-
-    def __helpers(self, factories):
-        return TestBuilder.HelperComposer( *[ self.__helper(factory) for factory in factories ] )
-
-    def group(self, *factories):
-        return self.__helpers(factories)        
-
-    def testFunction(self, function = None):
-        if function is None:
-            # Used as decorator
-            def wrapper(function):
-                self.__current().addChild( FunctionTest(self.__current(), function) )
-                return function
-        
-            return wrapper
-        else:
-            self.__current().addChild( FunctionTest(self.__current(), function) )
-
-    def fromReferenceImplementation(self, referenceImplementation, testedImplementation, contextString = None):
-        return TestBuilder.ReferenceImplementationHelper(self, self.__contextStack, referenceImplementation, testedImplementation, contextString=contextString)
-        
-    def runTests(self):
-        self.tests()
-        assert len(self.__contextStack) == 1, "Context stack bug"
-        return self.__current().score()
-
-    class ReferenceImplementationHelper:
-        def __init__(self, parent, stack, referenceImplementation, testedImplementation, contextString):
-            self.__parent = parent
-            self.__stack = stack
-            self.__refImpl = referenceImplementation
-            self.__testedImpl = testedImplementation
-            self.__contextString = contextString or "Called function with arguments ({inputs})"
-                    
-
-        def __call__(self, *args, **kwargs):
-            expectedResult = self.__refImpl(*args, **kwargs)
-            positionalArgsStrings = list(map(str, args))
-            keywordArgsStrings = [ "{}={}".format(key, val) for key, val in kwargs ]
-            contextMessage = self.__contextString.format(inputs=",".join(positionalArgsStrings + keywordArgsStrings))
-            
-            def tester(ensure):
-                actualResult = self.__testedImpl(*args, **kwargs)
-                ensure.equals(expectedResult, actualResult)
-
-            with self.__parent.group(context(contextMessage)):
-                self.__parent.testFunction(function = tester)
-
-        def __enter__(self):
-            self.__stack.append( cumulative()( self.__stack[-1] ) )
-
-            return self
-
-        def __exit__(self, type, value, traceback):
-            top = self.__stack.pop()
-            self.__stack[-1].addChild(top)
-            
-
-    class ContextStackHelper:
-        def __init__(self, stack, factory):
-            self.stack = stack
-            self.factory = factory
+    print("FAIL: {}".format(_testenv.testName))
     
-        def __enter__(self):
-            self.stack.append(self.factory(self.stack[-1]))
-            
-        def __exit__(self, type, value, traceback):
-            top = self.stack.pop()
-            self.stack[-1].addChild(top)
+    if len(errorStackFrame.code_context) > 1:
+        print("FAIL: {}".format(_testenv.testName))
+        print("In function {function} ({file}, line {line}):".format(file = errorStackFrame.filename, line = errorStackFrame.lineno, function = errorStackFrame.function))
+        print("\n".join([ "  " + line.strip() for line in errorStackFrame.code_context ]))
+    else:
+        print("In function {function} ({file}, line {line}): {code}".format(file = errorStackFrame.filename, line = errorStackFrame.lineno, function = errorStackFrame.function, code = errorStackFrame.code_context[0].strip()))
 
-    class HelperComposer:
-        def __init__(self, *components):
-            self.__components = components
+    print("Additional information:")
+    for item in _testenv.context:
+        print("  " + item)
 
-        def __enter__(self):
-            for component in self.__components:
-                component.__enter__()
+    print("")
+    raise TestFailure()
 
-        def __exit__(self, type, value, traceback):
-            for component in reversed(self.__components):
-                component.__exit__(type, value, traceback)
-
-class Ensure:
-    def __init__(self, parent):
-        self.__parent = parent
-
-    def fail(self, message, *args):
-        print(self.__parent)
-        print(message.format(*args))
-        print('---')
-        raise FailureError()
-
-    def equals(self, expected, actual):
+def mustBeEqual(expected, actual):
+    with context("Expected value: {}", expected), context("Actual value: {}", actual):
         if expected != actual:
-            self.fail("Expected {}, received {}", expected, actual)
+            fail()
 
-    def true(self, value):
-        self.equals(True, value)
+def mustBeSameTruthiness(expected, actual):
+    if expected:
+        with context("Expected {} to be truthy", actual):
+            if not actual:
+                fail()
+    else:
+        with context("Expected {} to be falsey", actual):
+            if actual:
+                fail()
+            
+def mustBeTruthy(actual):
+    with context("Value that should be true: {}", actual):
+        if not actual:
+            fail()
 
-    def false(self, value):
-        self.equals(False, value)
+def mustBeFalsey(actual):
+    with context("Value that should be false: {}", actual):
+        if actual:
+            fail()
+
+def ignore(*args, **kwargs):
+    pass
+
+            
+def reftest(name, result = None, arguments = None):
+    compareResults = result or mustBeEqual
+    compareArguments = arguments or ignore
+    
+    def testFunction(*args, **kwargs):
+        assert _testenv.isBound("testedFunction"), "No test function set. Use testedModule and testedFunctionName."
+        assert _testenv.isBound("referenceFunction"), "No reference function set. Use referenceModule and testedFunctionName."
+    
+        @test(name)
+        def referenceImplementationTest():
+            testargs = copy.deepcopy(args)
+            testkwargs = copy.deepcopy(kwargs)
+            refargs = copy.deepcopy(args)
+            refkwargs = copy.deepcopy(kwargs)
+
+            refretval = _testenv.referenceFunction(*refargs, **refkwargs)
+            testretval = _testenv.testedFunction(*testargs, **testkwargs)
+
+            with context("Comparing return values"):
+                compareResults(refretval, testretval)
+
+            for i in range(0, len(args)):
+                with context("Comparing positional argument #{}", i):
+                    compareArguments(refargs[i], testargs[i])
+
+            for key in kwargs:
+                with context("Comparing keyword argument {}", key):
+                    compareArguments(refkwargs[key], testkwargs[i])
+
+    return testFunction
+            
+            
+def runTests():
+    score = _testenv.top.run()
+    print("Passed tests: {}".format(len(_testenv.passedTests)))
+    print("Failed tests: {}".format(len(_testenv.failedTests)))
+    print("Skipped tests: {}".format(len(_testenv.skippedTests)))
+    print("Score: {}".format(score))
 
