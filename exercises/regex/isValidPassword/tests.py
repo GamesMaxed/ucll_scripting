9c5d139d9187ea4ef28c9ddc489977ac53a0da53
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-22 09:56:43 +0100

    exercises/regex/isValidPassword

--- exercises/regex/isValidPassword/tests.py
+++ exercises/regex/isValidPassword/tests.py
@@ -3,43 +3,34 @@ import student
 from testing import *
 
 
-class PrimeTests(TestBuilder):
-    def __init__(self):
-        super().__init__(student)
+with testedModule(student), referenceModule(solution), cumulative:
+    with testedFunctionName("isValidPassword"):
+        invalid_passwords = [ 'a', 'aaaaaaaa', '7aTkffff', '7a!kqfpoifpo', 'a!kJFLoifpo' ]    
+        valid_passwords = [ '7aT!ffff', '7 aA!         ', 'aBc123!!' ]
 
-    def tests(self):
-        with self.group(cumulative()):
-            def mustBeValid(password):
-                with self.group(context('{} should be considered valid', password)):
-                    self.testFunction(lambda ensure: ensure.true(student.isValidPassword(password)))
+        for invalid_password in invalid_passwords:
+            @test("\"{}\" must be an invalid password", invalid_password)
+            def checkInvalidPassword(invalid_password=invalid_password):
+                isValidPassword = testedFunction()
+                mustBeFalsey(isValidPassword(invalid_password))
 
-            def mustBeInvalid(password):
-                with self.group(context('{} should be considered invalid', password)):
-                    self.testFunction(lambda ensure: ensure.false(student.isValidPassword(password)))
-
-            mustBeInvalid('a')
-            mustBeInvalid('aaaaaaaa')
-            mustBeValid('7aT!ffff')
-            mustBeInvalid('7aTkffff')
-            mustBeInvalid('7a!kqfpoifpo')
-            mustBeInvalid('a!kJFLoifpo')
-            mustBeValid('7 aA!         ')
-            
-            with self.fromReferenceImplementation(solution.isValidPassword, student.isValidPassword, contextString="isPrime({inputs})") as addCase:
-                addCase('jf7!fjifjid')
-                addCase('jfF!fjifjidd')
-                addCase('78465163546546')
-                addCase('$#$!*((*$(')
-                addCase('                  ')
-                addCase('ffjlHJLKHffwq')
-                addCase('16!ffJKLJdd ')
-                addCase('ab7!D')
+        for valid_password in valid_passwords:
+            @test("\"{}\" must be a valid password", valid_password)
+            def checkValidPassword(valid_password=valid_password):
+                isValidPassword = testedFunction()
+                mustBeTruthy(isValidPassword(valid_password))
 
 
+        for password in [ 'jf7!fjifjid',        \
+                          'jfF!fjifjidd',       \
+                          '78465163546546',     \
+                          '$#$!*((*$(',         \
+                          '                  ', \
+                          'ffjlHJLKHffwq',      \
+                          '16!ffJKLJdd ',       \
+                          'ab7!D' ]:
+            reftest('Verifying isValidPassword("{}")'.format(password), result=mustBeSameTruthiness)(password)
 
-def main():
-    finalGrade = PrimeTests().runTests()
-    print("Final grade: {0:.1f}/{1:2}".format(finalGrade.value, finalGrade.maximum))
 
-
-main()
+if __name__ == '__main__':
+    runTests()
