a0a28a621458e1c3d4f11f53b87562e4f6e1b255
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-22 21:43:47 +0100

    exercises/regex/containsTwice

--- exercises/regex/containsTwice/tests.py
+++ exercises/regex/containsTwice/tests.py
@@ -1,37 +1,27 @@
-import solution
-import student
 from testing import *
+from testing.tests import *
+from testing.assertions import *
 
 
-class PrimeTests(TestBuilder):
-    def __init__(self):
-        super().__init__(student)
+with cumulative, testedFunctionName("containsTwice"):
+    def yes(string, fragment):
+        @test("Checking if {} contains {} twice; expected true", repr(string), repr(fragment))
+        def checkContainsTwice():
+            mustBeTruthy(testedFunction(string, fragment))
 
-    def tests(self):
-        with self.group(cumulative()):
-            def mustBeTrue(text, fragment):
-                with self.group(context('{} contains {} twice', text, fragment)):
-                    self.testFunction(lambda ensure: ensure.true(student.containsTwice(text, fragment)))
-
-            def mustBeFalse(text, fragment):
-                with self.group(context('{} does not contain {} twice', text, fragment)):
-                    self.testFunction(lambda ensure: ensure.false(student.containsTwice(text, fragment)))
-
-            mustBeTrue('aa', 'a')
-            mustBeTrue('axa', 'a')
-            mustBeFalse('aaa', 'aa')
-            mustBeTrue('abcabc', 'abc')
-            mustBeTrue('abcxyzabc', 'abc')
-            mustBeFalse('abcxyzab', 'abc')
-            mustBeFalse('abxyzabc', 'abc')
-            mustBeTrue('ababab', 'ab')
-            mustBeFalse('', 't')
-            mustBeTrue('', '')            
-
-
-def main():
-    finalGrade = PrimeTests().runTests()
-    print("Final grade: {0:.1f}/{1:2}".format(finalGrade.value, finalGrade.maximum))
-
-
-main()
+    def no(string, fragment):
+        @test("Checking if {} contains {} twice; expected false", repr(string), repr(fragment))
+        def checkNotContainsTwice():
+            mustBeFalsey(testedFunction(string, fragment))
+                    
+    yes('aa', 'a')
+    yes('axa', 'a')
+    no('aaa', 'aa')
+    yes('abcabc', 'abc')
+    yes('abcxyzabc', 'abc')
+    no('abcxyzab', 'abc')
+    no('abxyzabc', 'abc')
+    yes('ababab', 'ab')
+    no('', 't')
+    yes('', '')            
+        
