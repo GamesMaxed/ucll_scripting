d096c0dcaed177d272751c63f964e4aa52094de8
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-23 22:03:43 +0100

    exercises/containers/longest-increasing-subsequence

---
+++ exercises/containers/longest-increasing-subsequence/solution.py
@@ -0,0 +1,17 @@
+def longestIncreasingSubsequence(xs):
+    if len(xs) == 0:
+        return []
+    else:
+        longest = []
+        current = [xs[0]]
+    
+        for x in xs[1:]:
+            if x < current[-1]:
+                current = []
+                
+            current.append(x)
+
+            if len(current) > len(longest):
+                longest = current
+
+        return longest
---
+++ exercises/containers/longest-increasing-subsequence/student.py
@@ -0,0 +1,7 @@
+def longestIncreasingSubsequence(xs):
+    """
+    Finds the longest increasing subsequence.
+    In case of a tie, returns the leftmost one.
+    """
+    
+    raise RuntimeError("Not yet implemented")
