638f51beab9c34930a254b5015691981ad650e5d
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-23 22:17:13 +0100

    exercises/containers/longest-increasing-subsequence

--- exercises/containers/longest-increasing-subsequence/tests.py
+++ exercises/containers/longest-increasing-subsequence/tests.py
@@ -4,13 +4,15 @@ from testing.assertions import *
 
 
 with cumulative, testedFunctionName("longestIncreasingSubsequence"):
-    reftest()([])
-    reftest()([0])
-    reftest()([0,1,2,3,4,5])
-    reftest()([5,1,2,3,0])
-    reftest()([1,2,3,2,3,4])
-    reftest()([1,5,2,6,3,7,4,8,5,9])
-    reftest()([1,3,5,7,9,2,4,6,8])
-    reftest()([1,3,5,7,9,2,4,6,8,10,12])
-    reftest()([50,55,58,40,41,42,43,45,48,49,10,15,18])
+    check = reftest()
+
+    check([])
+    check([0])
+    check([0,1,2,3,4,5])
+    check([5,1,2,3,0])
+    check([1,2,3,2,3,4])
+    check([1,5,2,6,3,7,4,8,5,9])
+    check([1,3,5,7,9,2,4,6,8])
+    check([1,3,5,7,9,2,4,6,8,10,12])
+    check([50,55,58,40,41,42,43,45,48,49,10,15,18])
 
