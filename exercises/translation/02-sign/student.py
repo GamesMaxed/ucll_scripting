7265ca07f96705ace320676fbc2302e862f1682d
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-24 12:49:28 +0100

    exercises/translation/02-sign

---
+++ exercises/translation/02-sign/student.py
@@ -0,0 +1,4 @@
+# Zoek de syntax op voor de if-statement
+# Merk op dat 'else if' in Python een speciale syntax heeft
+def sign(x):
+    raise RuntimeError("Not yet implemented")
---
+++ exercises/translation/02-sign/tests.py
@@ -0,0 +1,10 @@
+from testing import *
+from testing.tests import *
+from testing.assertions import *
+
+
+with allOrNothing, testedFunctionName("sign"):
+    check = reftest()
+
+    for i in range(-10,10):
+        check(i)
