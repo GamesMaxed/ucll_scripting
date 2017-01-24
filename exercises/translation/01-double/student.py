743ffd304411e38777c50beaf770c4e16f5d8d03
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-24 12:42:28 +0100

    exercises/translation/01-double

--- exercises/translation/01-double/student.py
+++ exercises/translation/01-double/student.py
@@ -1,2 +1,8 @@
+# Python laat "losse functies" toe, d.i. functies die niet behoren tot een klasse
+# Merk op dat in Python er slechts een enkele square functie nodig is:
+# in Java is het nodig per vermenigvuldigbaar type (int, double, ...) een aparte square-methode
+# te schrijven, terwijl in Python een enkele functie kan werken met alle vermenigvuldigbare waarden.
 def square(x):
+    # Vervang onderstaande lijn door de vertaling
+    # van de Java-code
     raise RuntimeError("Not yet implemented")
