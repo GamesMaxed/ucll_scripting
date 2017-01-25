5cf91374924a890496e814c0923e69a9ebdb46d4
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-25 13:07:59 +0100

    exercises/translation/01-double

--- exercises/translation/01-double/tests.py
+++ exercises/translation/01-double/tests.py
@@ -3,7 +3,7 @@ from testing.tests import *
 from testing.assertions import *
 
 
-with allOrNothing, testedFunctionName("square"):
+with allOrNothing(), testedFunctionName("square"):
     check = reftest()
 
     for i in range(-10,10):
