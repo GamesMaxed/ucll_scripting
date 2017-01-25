f28dccf89947e0292703f4a8a33df91da83656f8
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-25 13:08:05 +0100

    exercises/translation/03-sum-up-to

--- exercises/translation/03-sum-up-to/tests.py
+++ exercises/translation/03-sum-up-to/tests.py
@@ -3,7 +3,7 @@ from testing.tests import *
 from testing.assertions import *
 
 
-with allOrNothing, testedFunctionName("sumUpTo"):
+with allOrNothing(), testedFunctionName("sumUpTo"):
     check = reftest()
 
     for i in range(0,10):
