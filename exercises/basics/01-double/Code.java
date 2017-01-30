8a0a06bbf82681103d8d2d5441ef53fd547c9224
Frederic Vogels <frederic.vogels@gmail.com>
2017-01-30 09:21:25 +0100

    exercises/basics/01-double

---
+++ exercises/basics/01-double/Code.java
@@ -0,0 +1,33 @@
+class Code
+{
+    /*
+      De vertaling van deze methode krijg je cadeau.
+      Gebruik het als inspiratie om square te vertalen.
+     */
+    public int increment(int x)
+    {
+        return x + 1;
+    }
+    
+    
+    /*
+      Java heeft aparte methodes nodig om ints en doubles
+      te kunnen kwadrateren.
+      In Python kan square voorgesteld door een enkele functie.
+
+      Java ondersteunt geen "losse" functies: ze moeten altijd
+      horen bij een klasse. Python laat daarentegen
+      wel "losse" functies toe. Implementeer
+      square als zulk een functie.
+     */
+    
+    public int square(int x)
+    {
+        return x * x;
+    }
+
+    public double square(double x)
+    {
+        return x * x;
+    }
+}
--- exercises/basics/01-double/student.py
+++ exercises/basics/01-double/student.py
@@ -1,8 +1,7 @@
-# Python laat "losse functies" toe, d.i. functies die niet behoren tot een klasse
-# Merk op dat in Python er slechts een enkele square functie nodig is:
-# in Java is het nodig per vermenigvuldigbaar type (int, double, ...) een aparte square-methode
-# te schrijven, terwijl in Python een enkele functie kan werken met alle vermenigvuldigbare waarden.
+def increment(x):
+    return x + 1
+
+
 def square(x):
-    # Vervang onderstaande lijn door de vertaling
-    # van de Java-code
+    # Vervang onderstaande lijn door de vertaling van de Java-code
     raise NotImplementedError()
