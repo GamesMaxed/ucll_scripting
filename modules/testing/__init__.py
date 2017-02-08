101a1b58de9a08f471a6cb0a9e2115ba4e617d13
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-08 21:26:24 +0100

    modules/testing

--- modules/testing/__init__.py
+++ modules/testing/__init__.py
@@ -4,7 +4,7 @@ def check_python_version():
     version = sys.version_info
 
     if version.major < 3 or (version.major == 3 and version.minor < 5):
-        sys.exit("You need at least Python 3.5")
+        sys.exit("You need at least Python 3.5. You have version {}.{}...".format(version.major, version.minor))
 
 check_python_version()
 
