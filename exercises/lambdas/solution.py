82d19cf28b91e7b76709bea073c295ab3d90e137
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-08 01:52:00 +0100

    exercises/lambdas

--- exercises/lambdas/solution.py
+++ exercises/lambdas/solution.py
@@ -14,3 +14,17 @@ def find_first(xs, predicate, default = None):
             return x
 
     return default
+
+
+def group_by_key(xs, key_function):
+    result = {}
+
+    for x in xs:
+        key = key_function(x)
+
+        if key not in result:
+            result[key] = []
+
+        result[key].append(x)
+
+    return result
--- exercises/lambdas/tests.py
+++ exercises/lambdas/tests.py
@@ -5,35 +5,49 @@ from testing.util import *
 
 
 with cumulative():
-    alwaysTrue = named_lambda('alwaysTrue', lambda x: True)
-    alwaysFalse = named_lambda('alwaysFalse', lambda x: False)
-    isOdd = named_lambda('isOdd', lambda x: x % 2 != 0)
-    isInt = named_lambda('isOdd', lambda x: type(x) == int)
+    always_true = named_lambda('always_true', lambda x: True)
+    always_false = named_lambda('always_false', lambda x: False)
+    is_odd = named_lambda('is_odd', lambda x: x % 2 != 0)
+    is_int = named_lambda('is_odd', lambda x: type(x) == int)
+
+    get_type = named_lambda('get_type', lambda x: type(x).__name__)
+    
 
 
     with tested_function_name('count'), all_or_nothing():
         count = reftest()
 
-        count([], alwaysTrue)
-        count([1], alwaysTrue)
-        count([1, 2, 3], alwaysTrue)
-        count([], alwaysFalse)
-        count([1], alwaysFalse)
-        count([1, 2, 3], alwaysFalse)
-        count([1, 2, 3], isOdd)
-        count([1, 2, 3, 4, 5], isOdd)
-        count([1, 2, 3, 4, 5], isInt)
-        count(['x', 1, '4', 2], isInt)
+        count([], always_true)
+        count([1], always_true)
+        count([1, 2, 3], always_true)
+        count([], always_false)
+        count([1], always_false)
+        count([1, 2, 3], always_false)
+        count([1, 2, 3], is_odd)
+        count([1, 2, 3, 4, 5], is_odd)
+        count([1, 2, 3, 4, 5], is_int)
+        count(['x', 1, '4', 2], is_int)
 
     with tested_function_name('find_first'), all_or_nothing():
         find_first = reftest()
 
-        find_first([], alwaysTrue, 1)
-        find_first([], alwaysTrue, None)
-        find_first([], alwaysTrue, 'default')
-        find_first([1], alwaysTrue, 1)
-        find_first([1, 2], alwaysTrue, 1)
-        find_first([1, 2, 3], alwaysTrue, 1)
-        find_first([1], alwaysFalse, 7)
-        find_first([1, 2], alwaysFalse, 4)
-        find_first([1, 2, 3], alwaysFalse, 99)
+        find_first([], always_true, 1)
+        find_first([], always_true, None)
+        find_first([], always_true, 'default')
+        find_first([1], always_true, 1)
+        find_first([1, 2], always_true, 1)
+        find_first([1, 2, 3], always_true, 1)
+        find_first([1], always_false, 7)
+        find_first([1, 2], always_false, 4)
+        find_first([1, 2, 3], always_false, 99)
+
+    with tested_function_name('group_by_key'), all_or_nothing():
+        group_by_key = reftest()
+
+        group_by_key([], get_type)
+        group_by_key([1], get_type)
+        group_by_key([1, 2], get_type)
+        group_by_key(['x'], get_type)
+        group_by_key([1, 'x'], get_type)
+        group_by_key([1, 'x', [1,2], {1,2}], get_type)
+        group_by_key([1, 2, 3, 4, 5], is_odd)
