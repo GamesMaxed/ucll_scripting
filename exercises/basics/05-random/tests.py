2ff31e7e86d7fc2a522a7df6e77b123aae7c4a50
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-08 01:06:12 +0100

    exercises/basics/05-random

--- exercises/basics/05-random/tests.py
+++ exercises/basics/05-random/tests.py
@@ -5,10 +5,7 @@ from testing.assertions import *
 
 with cumulative():
     with tested_function_name("probability_of_sum_higher_than"), all_or_nothing():
-        def must_be_approx(x, y):
-            must_be_equal(x, y, epsilon=0.01)
-            
-        probability_of_sum_higher_than = reftest(result=must_be_approx)
+        probability_of_sum_higher_than = reftest(result=must_be_equal.with_epsilon(0.01))
 
         probability_of_sum_higher_than(1, 0, 1)
         probability_of_sum_higher_than(1, 7, 1)
