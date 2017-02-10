7c615f763026c5b71334d99b00713b05d5975599
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-10 12:41:43 +0100

    exercises/basics/00-demo

--- exercises/basics/00-demo/tests.py
+++ exercises/basics/00-demo/tests.py
@@ -3,9 +3,7 @@ from testing.tests import *
 from testing.assertions import *
 
 with cumulative(), tested_function_name('demo'):
-    @test('Demo test')
-    def _():
-        demo = reftest()
+    demo = reftest()
 
-        for i in range(1, 11):
-            demo(i)
+    for i in range(1, 11):
+        demo(i)
