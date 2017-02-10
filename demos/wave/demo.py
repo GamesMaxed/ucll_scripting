b739271dc3cdc448df9a1362c74b087ee450ac28
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-10 23:30:54 +0100

    demos/wave

--- demos/wave/demo.py
+++ demos/wave/demo.py
@@ -1,8 +1,14 @@
 import wave
+import sys
 
 
 with wave.open('bojack.wav', 'rb') as file:
     with wave.open('test.wav', 'wb') as out:
+        params = file.getparams()
+
+        if params.nchannels != 1 or params.sampwidth != 1:
+            sys.exit('wav file should be mono 8 bit')
+        
         nframes = file.getnframes()
         out.setparams( (1, 1, 44100, nframes // 2, 'NONE', 'not compressed') )
 
