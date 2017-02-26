02be9d594cd94b4e3cdb09a5679f7551cfc48096
Frederic Vogels <frederic.vogels@gmail.com>
2017-02-26 14:00:15 +0100

    exercises/io/morse

--- exercises/io/morse/student.py
+++ exercises/io/morse/student.py
@@ -6,6 +6,12 @@ from functools import reduce
 def decode_morse(code):
     """
     Decodeert de gegeven morsecode.
+    De morsecode bestaan uit '.' en '-'
+    en de groepjes tekens die overeenkomen
+    met een enkele letter of getal
+    worden gescheiden door een spatie.
+    Bv. '... --- ...' voor SOS (en niet '...---...')
+
     Steunt op het bestand morse.txt om
     de morsecodes in te lezen.
     """
@@ -16,8 +22,9 @@ def decode_morse(code):
 def encode_morse(plaintext):
     """
     Zet de gegeven string om naar plaintext.
-    Steunt op het bestand morse.txt om
-    de morsecodes in te lezen.
+    De string bestaat uitsluitend uit
+    tekens die ook voortkomen in morse.txt,
+    m.a.w. er komen geen spaties in voor.
     """
 
     raise NotImplementedError()
