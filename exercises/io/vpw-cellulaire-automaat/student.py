5b0c38e700a2b56a6006679654b890875fe898b4
Frederic Vogels <frederic.vogels@gmail.com>
2017-03-09 10:25:58 +0100

    exercises/io/vpw-cellulaire-automaat

---
+++ exercises/io/vpw-cellulaire-automaat/student.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def next_generation(bs):
+    """
+    Rekent de volgende generatie uit voor de gegeven
+    string bs, die bestaat uit uitsluitend 0'en en 1'en.
+    """
+
+    raise NotImplementedError()
+
+
+def compute_n_generations(bs, n):
+    """
+    Rekent n generaties uit.
+    """
+    
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    raise NotImplementedError()
