4ba479eedd96d0744ab2139109d50635abf77f16
Robin Van den Broeck <robin.broeck@gmail.com>
2017-03-14 13:35:33 +0100

    Properties gemaakt

--- exercises/basics/07-properties/student.py
+++ exercises/basics/07-properties/student.py
@@ -1,19 +1,36 @@
 class Person:
     def __init__(self, weight, length):
-        raise NotImplementedError()
+        __weight = 0
+        __height = 0
+        self.weight = weight
+        self.height = length
 
     # Weight getter
     @property
     def weight(self):
-        raise NotImplementedError()
+        return self.__weight
 
     # Weight setter
     @weight.setter
     def weight(self, value):
-        raise NotImplementedError()
+        if value < 0:
+            raise RuntimeError()
+        self.__weight = value
 
     # Height getter
+    @property
+    def height(self):
+        return self.__height
 
     # Height setter
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise RuntimeError()
+        self.__height = value
+
 
     # Bmi getter
+    @property
+    def bmi(self):
+        return self.weight / self.height ** 2
