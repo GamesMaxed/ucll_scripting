07f527d2c4f70d5b5d08933f48309fa958a88982
Frederic Vogels <frederic.vogels@ucll.be>
2017-03-27 19:28:49 +0200

    topics/lambdas

--- topics/lambdas/selectAdults3.py
+++ topics/lambdas/selectAdults3.py
@@ -1,11 +1,11 @@
 def is_adult(person):
-    return `\pgfmark{selectAdults3 start}`person.age >= 18`\pgfmark{selectAdults3 end}`
+    return `\pgfmark{selectAdults3 age start}`person.age >= 18`\pgfmark{selectAdults3 age end}`
 
 def select_adults(persons):
     result = []
 
     for person in persons:
-        if `\pgfmark{selectAdults3b start}`is_adult(person)`\pgfmark{selectAdults3b end}`:
+        if `\pgfmark{selectAdults3 isadult start}`is_adult(person)`\pgfmark{selectAdults3 isadult end}`:
             result.append(person)
 
     return result
