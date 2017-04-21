8ece6559fe1a2f52614061c2779f3bfb580e5057
Frederic Vogels <frederic.vogels@gmail.com>
2017-04-21 14:46:22 +0200

    exercises/modules/argparse/print

---
+++ exercises/modules/argparse/print/student.py
@@ -0,0 +1,27 @@
+'''
+Schrijf een script dat als men het vanuit de shell opstart,
+elk command line argument op een aparte regel wordt afgeprint.
+
+Bv.
+
+    python student.py a b c d
+
+moet afprinten
+
+    a
+    b
+    c
+    d
+
+Let erop dat het script niet ook 'student.py' afprint.
+'''
+
+
+import sys
+
+
+# Deze if gaat na of het script vanop de command line wordt uitgevoerd
+# sys.argv bevat de command line arguments
+if __name__ == '__main__':
+    # TODO
+    
