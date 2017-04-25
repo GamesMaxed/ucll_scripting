0a8bf4bb507ef1a5b36a6854e9aac27c6b79d37b
Robin Van den Broeck <robin.broeck@gmail.com>
2017-04-25 14:10:17 +0200

    Added 10-sets

--- exercises/basics/10-sets/student.py
+++ exercises/basics/10-sets/student.py
@@ -1,20 +1,28 @@
-def create_interval(a, b):
-    result = set()
+from typing import List, Set
 
-    for i in range(a, b + 1):
-        result.add(i)
+
+def create_interval(a, b):
+    return set(range(a, b+1))
+
+
+def remove_duplicates_preserving_order(strings: List[str]) -> List[str]:
+    found = set()
+    result = list()
+
+    for string in strings:
+        if found.__contains__(string):
+            continue
+        found.add(string)
+        result.append(string)
 
     return result
 
-# Kortere implementatie
-def create_interval(a, b):
-    return set( range(a, b+1) )
-
-
-def remove_duplicates_preserving_order(strings):
-    raise NotImplementedError()
-
 
 # remove_duplicates_not_preserving_order
+def remove_duplicates_not_preserving_order(strings: List[str]) -> List[str]:
+    return list(set(strings))
+
 
 # count_unique
+def count_unique(strings: List[str]) -> int:
+    return len(set(strings))
