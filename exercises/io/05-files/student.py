354f646f0e21c34f58de4cd5e208390d8afe21ff
Robin Van den Broeck <robin.broeck@gmail.com>
2017-05-30 14:44:34 +0200

    Completed 'io/05-files'

--- exercises/io/05-files/student.py
+++ exercises/io/05-files/student.py
@@ -1,3 +1,6 @@
+from os import listdir
+from os.path import isfile, join
+
 def files(path):
     """
     Geeft een lijst van bestandsnamen terug in de gegeven directory.
@@ -6,15 +9,21 @@ def files(path):
     Zoek zelf op hoe je in Python een lijst van bestanden opvraagt
     en hoe je kan weten of het gaat om een bestand of een directory.
     """
+    return [filename for filename in listdir(path) if isfile(join(path, filename))]
 
-    raise NotImplementedError()
 
-
-# Indien er een command line argument werd meegeleverd,
-# stelt dit het pad voor waarin naar bestanden moeten worden
-# gezocht.
-# Indien er geen command line argument gegeven werd,
-# wordt er gekeken in de huidige directory.
-# Je kan verwijzen naar de huidige directory met '.'.
 if __name__ == '__main__':
-    # pass
+    """
+    Indien er een command line argument werd meegeleverd,
+    stelt dit het pad voor waarin naar bestanden moeten worden
+    gezocht.
+    Indien er geen command line argument gegeven werd,
+    wordt er gekeken in de huidige directory.
+    Je kan verwijzen naar de huidige directory met '.'.
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, default='.', help='Path to the directory')
+    path = parser.parse_args().path
+    print(*files(path), sep='\n')
