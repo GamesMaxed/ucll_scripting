f5362f2342f79fbf8cc6534aa7d50fc04e893591
Robin Van den Broeck <robin.broeck@gmail.com>
2017-05-30 15:38:13 +0200

    Added 'io/07-loc'

--- exercises/io/07-loc/student.py
+++ exercises/io/07-loc/student.py
@@ -1,11 +1,20 @@
 import sys
 import re
 import os
+import pathlib
 
 
+languages = {
+    '.py': '#',
+    '.rb': '#',
+    '.java': '//',
+    '.cpp': '//',
+    '.hs': '--',
+    '.tex': '%',
+    '.cl': '%'
+}
 
-
-def is_loc(line, comment_delimiter):
+def is_loc(line: str, comment_delimiter: str):
     """
     Gaat na of de string line meetelt als line of code (loc).
     Dit is het geval indien de string een niet-whitespace
@@ -19,22 +28,21 @@ def is_loc(line, comment_delimiter):
       is_loc('     x', '//') geeft True
       is_loc('    // klk', '//') geeft False omdat er buiten de commentaar enkel whitespace is
     """
+    line = line.strip()
+    return len(line) > 0 and not line.startswith(comment_delimiter)
 
-    raise NotImplementedError()
 
-
-def extension_of(filename):
+def extension_of(filename: str):
     """
     Geeft de extensie terug van het bestand.
     Het punt moet mee in het resultaat zitten.
     Bv. extension_of('bla.txt') geeft '.txt' als resultaat.
 
-    Deze functionaliteit is reeds geïmplementeerd
+    Deze functionaliteit is reeds geimplementeerd
     in een Python library. Zoek op waar (bv. google naar 'python get file extension')
     en maak er gebruik van.
     """
-
-    raise NotImplementedError()
+    return pathlib.Path(filename).suffix
     
 
 def is_source_file(filename):
@@ -42,8 +50,7 @@ def is_source_file(filename):
     Gaat na of het gegeven bestand source code bevat,
     afgaande op de extensie. Zie readme.txt.
     """
-
-    raise NotImplementedError()
+    return extension_of(filename) in languages.keys()
 
 
 def comment_delimiter_for(filename):
@@ -55,17 +62,15 @@ def comment_delimiter_for(filename):
       comment_delimiter_for('foo.py') geeft '#'
       comment_delimiter_for('foo.java') geeft '//'
     """
+    return languages[extension_of(filename)]
 
-    raise NotImplementedError()
 
-
-def loc(source, delimiter):
+def loc(source: str, delimiter: str):
     """
     Telt het aantal loc in de string source, ervan uitgaande
     dat commentaar begint met de gegeven delimiter.
     """
-
-    raise NotImplementedError()
+    return len([line for line in source.split('\n') if is_loc(line, delimiter)])
 
 
 def loc_in_file(filename):
@@ -74,8 +79,11 @@ def loc_in_file(filename):
     Indien het bestand geen source code bevat
     (zie is_source_file), dan geeft deze functie 0 terug.
     """
+    if not is_source_file(filename):
+        return 0
 
-    raise NotImplementedError()
+    with open(filename) as file:
+        return loc("".join(file.readlines()), languages[extension_of(filename)])
 
 
 def loc_in_directory(path):
@@ -86,13 +94,18 @@ def loc_in_directory(path):
     in alle subdirectories.
     Enkel source files komen in aanmerking.
     """
-    
-    raise NotImplementedError()
+    return sum(loc_in_directory(entry) if entry.is_dir() else loc_in_file(entry) for entry in os.scandir(path))
 
 
-# Het command line argument geeft het pad van de directory
-# aan waarin alle locs worden geteld.
-# Indien er geen command line argument geleverd werd,
-# wordt geteld in de huidige directory.
 if __name__ == '__main__':
-    raise NotImplementedError()
+    """
+    Het command line argument geeft het pad van de directory
+    aan waarin alle locs worden geteld.
+    Indien er geen command line argument geleverd werd,
+    wordt geteld in de huidige directory.
+    """
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, default='.', help='Path to the directory')
+    path = parser.parse_args().path
+    print(loc_in_directory(path))
