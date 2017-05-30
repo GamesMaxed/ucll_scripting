172f483cd877c405288574ca45cb012e7d2137be
Robin Van den Broeck <robin.broeck@gmail.com>
2017-05-30 15:17:47 +0200

    Completed 'io/06-tree'

--- exercises/io/06-tree/student.py
+++ exercises/io/06-tree/student.py
@@ -1,15 +1,33 @@
-def tree(path = '.'):
-    # Zie readme.txt
-    raise NotImplementedException()
+import os
+from typing import List
 
 
-# Indien opgeroepen vanuit shell,
-# moet het resultaat van tree(path)
-# afprinten, waarbij path meegegeven
-# wordt als command line argument.
-# Indien er geen command line argument
-# werd meegegeven, moet de tree
-# van de huidige directory ('.')
-# afgeprint worden.
+def tree(path: str) -> str:
+    def aux(path: str):
+        lines: List[str] = []
+        for entry in sorted(os.scandir(path), key=lambda x: x.name):
+            if entry.is_file():
+                lines.append(entry.name)
+            else:
+                lines.append("[{}]".format(entry.name))
+                lines += ["  " + children for children in aux(entry.path)]
+        return lines
+
+    return "\n".join(aux(path))
+
 if __name__ == '__main__':
-    raise NotImplementedException()
+    """
+    Indien opgeroepen vanuit shell,
+    moet het resultaat van tree(path)
+    afprinten, waarbij path meegegeven
+    wordt als command line argument.
+    Indien er geen command line argument
+    werd meegegeven, moet de tree
+    van de huidige directory ('.')
+    afgeprint worden.
+    """
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, default='.', help='Path to the directory')
+    path = parser.parse_args().path
+    print(tree(path))
